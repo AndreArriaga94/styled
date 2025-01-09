5ebc35356424873c42ed74b620649b3f467e906b
Ismar André Arriaga Lemus <arriaga109407@gmail.com>
2025-01-08 19:59:33 -0600

    se crea una app con styled components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const P = styled.p`
+  font-size: 24px;
+  color: red;
+`
+
+function App() {
+
+  return (
+    <div>
+      <P>Hola soy un parrafo</P>
+    </div>
+  )
+}
+
+export default App

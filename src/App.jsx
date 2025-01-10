e2d9fe6a61599d767cbfb89eaa4da364dcc4c4d2
AndreArriaga94 <arriaga109407@gmail.com>
2025-01-10 16:10:00 -0600

    se agregan los componentes de boton y link con ejemplos dinamicos de propiedades y con ejemplos de herencia de propiedades

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,43 @@ const P = styled.p`
   color: red;
 `
 
+const Content = styled.div`
+  padding: 20px 25px;
+` 
+const Button = styled.button`
+  background-color: ${props => props.primary ? 'red' : 'white'};
+  color: ${props => props.primary ? 'white' : 'red'};
+  padding: 10px 15px;
+  border: solid 2px red;
+  border-radius: 4px;
+` 
+
+const BlockButton = styled(Button)`
+  width: 100%;
+  font-size: 24px;
+`
+
+const Link = ({ className, ...props }) => {
+  return <a className={className} {...props} />
+}
+
+const StyledLink = styled(Link)`
+  color: blue;
+`
+
 function App() {
 
   return (
-    <div>
+    <Content>
       <P>Hola soy un parrafo</P>
-    </div>
+      <Button>Enviar</Button>
+      <Button primary>Enviar</Button>
+      <BlockButton>Enviar</BlockButton>
+      <BlockButton primary>Enviar</BlockButton>
+      <BlockButton primary as='a' href='#'>Enviar</BlockButton>
+      <Link>Link</Link>
+      <StyledLink>Link con stilo</StyledLink>
+    </Content>
   )
 }
 
